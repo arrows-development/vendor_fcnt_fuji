52f7120895339e3bf5c8804e52e90d1d3042eb90
Sarthak Roy <sarthakroy2002@gmail.com>
2025-09-30 22:08:30 +0900

    fuji: Ship full firmware with zip

--- Android.mk
+++ Android.mk
@@ -3,3 +3,31 @@
 #
 
 LOCAL_PATH := $(call my-dir)
+
+ifeq ($(TARGET_DEVICE),fuji)
+
+$(call add-radio-file-sha1-checked,radio/apusys.img,2c826c22f9e259e01bee2029ab1ca2e0f0ba753f)
+$(call add-radio-file-sha1-checked,radio/audio_dsp.img,8dd977a68bb1cc00b5b0eb036b9c54c0d15487d9)
+$(call add-radio-file-sha1-checked,radio/ccu.img,2593cba4ac5ce33a412fe234629dc6abf7c0279a)
+$(call add-radio-file-sha1-checked,radio/connsys_bt.img,f956994066e2743ad412cbc9a1bd8c260169b22f)
+$(call add-radio-file-sha1-checked,radio/connsys_gnss.img,513b7e7769a8a7f643d23c6d542a92be89e8d475)
+$(call add-radio-file-sha1-checked,radio/connsys_wifi.img,ea73852a8493edbc19bb533ca8f5715a4ba99cec)
+$(call add-radio-file-sha1-checked,radio/dpm.img,dee16e789b3c2691320f1563c303f89751f887b1)
+$(call add-radio-file-sha1-checked,radio/dtbo.img,a96acf933940a7cd276ab00596b86cdcbb8690a1)
+$(call add-radio-file-sha1-checked,radio/gpueb.img,a1db94550b42f8bba695b9dfe15c782a358220c9)
+$(call add-radio-file-sha1-checked,radio/gz.img,c7ac0a16279152fe80b97e263f14c60024d7e40b)
+$(call add-radio-file-sha1-checked,radio/lk.img,d51dc023e3d10254ceba91fc954097e666a09470)
+$(call add-radio-file-sha1-checked,radio/logo.img,12c9ad11fcf18b22dc554d0ae9d1b82eaeb3b50a)
+$(call add-radio-file-sha1-checked,radio/mcf_ota.img,a2398ad76fb0cbf36d87479d898d40bfe6018857)
+$(call add-radio-file-sha1-checked,radio/mcupm.img,ae44bf324ad8655a269cfbbe3c98ab3631363d27)
+$(call add-radio-file-sha1-checked,radio/modem.img,ca27cf70dabaa2734a824fb9213f508b13a2ff93)
+$(call add-radio-file-sha1-checked,radio/mvpu_algo.img,cbdda14aad4a456d1ceb084341db4e0511822fbf)
+$(call add-radio-file-sha1-checked,radio/pi_img.img,6a7bf652bafde44ad913748449d543bd518df2a4)
+$(call add-radio-file-sha1-checked,radio/preloader.img,1cc7335800de7091ca3874a5afef28114589b116)
+$(call add-radio-file-sha1-checked,radio/scp.img,5d89489015c5a1b62f9668979310aaf924bc554e)
+$(call add-radio-file-sha1-checked,radio/spmfw.img,219d576c3d9c68a34200700a473980e61a97757d)
+$(call add-radio-file-sha1-checked,radio/sspm.img,618e5165e52edd7d584fd13c6077ad2a548beb01)
+$(call add-radio-file-sha1-checked,radio/tee.img,ea6b69ee5d739c568980fdba08cc77887edf18f3)
+$(call add-radio-file-sha1-checked,radio/vcp.img,2e077d6949ae5c4032ce0754ea528199176329eb)
+
+endif
